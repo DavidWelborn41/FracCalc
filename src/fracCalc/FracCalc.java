49401ab2f0982d95922d14869cec9af03de9e295
David Welborn <david.welborn41@gmail.com>
2015-11-17 08:57:07 -0500

    Put in the scanner

--- src/fracCalc/FracCalc.java
+++ src/fracCalc/FracCalc.java
@@ -1,10 +1,11 @@
 package fracCalc;
-
+import java.util.Scanner;
 public class FracCalc {
 
     public static void main(String[] args) 
     {
-        // TODO: Read the input from the user and call produceAnswer with an equation
+        Scanner input = new Scanner(System.in);
+    	// TODO: Read the input from the user and call produceAnswer with an equation
 
     }
     
